929c489e87bce458f1058227131d54deec1f0582
DiestroCorleone <matias.perez.mcd@gmail.com>
2022-01-31 17:43:27 -0300

    First commit.

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+    return <nav className="nav">
+                <img src="../images/react-icon-small.png" />
+                <h2>ReactFacts</h2>
+                <h3>React Course - Project 1</h3>
+            </nav>
+}
